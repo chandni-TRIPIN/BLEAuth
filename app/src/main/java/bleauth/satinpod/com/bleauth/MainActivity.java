b8ea160c7330e7fe6f603ad0b201d78baf4a5053
ravishankarahirwar <ravishankar.ahirwar@gmail.com>
2018-07-17 14:27:52 +0530

    Still in progress mode

--- app/src/main/java/bleauth/satinpod/com/bleauth/MainActivity.java
+++ app/src/main/java/bleauth/satinpod/com/bleauth/MainActivity.java
@@ -20,41 +20,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 private final static String TAG = "BLE";
     private BluetoothAdapter mBluetoothAdapter;
     BluetoothDevice mBluetoothDevice;
-    private boolean mScanning;
     private Handler mHandler;
 
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
     private BluetoothAdapter.LeScanCallback mLeScanCallback;
 
-    private BluetoothManager mBluetoothManager;
-    private String mBluetoothDeviceAddress;
     private BluetoothGatt mBluetoothGatt;
-    private int mConnectionState = STATE_DISCONNECTED;
-
-    private static final int STATE_DISCONNECTED = 0;
-    private static final int STATE_CONNECTING = 1;
-    private static final int STATE_CONNECTED = 2;
 
     public final static String ACTION_GATT_CONNECTED =
             "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
     public final static String ACTION_GATT_DISCONNECTED =
             "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
-    public final static String ACTION_GATT_SERVICES_DISCOVERED =
-            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
-    public final static String ACTION_DATA_AVAILABLE =
-            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
-    public final static String EXTRA_DATA =
-            "com.example.bluetooth.le.EXTRA_DATA";
     private BluetoothGattCallback mGattCallback ;
 
     private BluetoothGattCharacteristic mCharIdentify;
@@ -141,14 +125,11 @@ private final static String TAG = "BLE";
                 String intentAction;
                 if (newState == BluetoothProfile.STATE_CONNECTED) {
                     intentAction = ACTION_GATT_CONNECTED;
-                    mConnectionState = STATE_CONNECTED;
                     Log.i(TAG, "Connected to GATT server.");
                     Log.i(TAG, "Attempting to start service discovery:" +
                             mBluetoothGatt.discoverServices());
-
                 } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                    intentAction = ACTION_GATT_DISCONNECTED;
-                    mConnectionState = STATE_DISCONNECTED;
+
                     Log.i(TAG, "Disconnected from GATT server.");
                 }
 
@@ -164,6 +145,7 @@ private final static String TAG = "BLE";
             @Override
             public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
                 super.onCharacteristicRead(gatt, characteristic, status);
+                broadcastUpdate(characteristic);
                 Log.v(TAG, "onCharacteristicRead" + characteristic.getUuid() + " Value : " + characteristic.getValue());
                 final byte[] data = characteristic.getValue();
                 if (data != null && data.length > 0) {
@@ -179,21 +161,17 @@ private final static String TAG = "BLE";
             public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
                 super.onCharacteristicWrite(gatt, characteristic, status);
 
-                Log.v(TAG, "onCharacteristicWrite : " + characteristic.getUuid() + " Status : " + status);
+                if (status == BluetoothGatt.GATT_SUCCESS)
+                {
+                    Log.e("WRITE SUCCESS", "onCharacteristicWrite() - status: " + status + "  - UUID: " + characteristic.getUuid());
 
-                boolean statusCharAuthRead = mBluetoothGatt.readCharacteristic(mCharAuthentication);
-                Log.v(TAG, "statusCharAuthRead Status : " + statusCharAuthRead);
+                } else {
 
-                    boolean statusNotifiaction = mBluetoothGatt.setCharacteristicNotification(mCharAuthentication, true);
-                    Log.v(TAG, "CharAuthentication Notification : " + statusNotifiaction);
-
+                }
                     if (mBluetoothDevice.getBondState() != BluetoothDevice.BOND_BONDED) {
 //                        mBluetoothDevice.setPin(new byte[]{ 0x60, 0x00, 0x60, 0x00});
                         boolean statusCreateBound = mBluetoothDevice.createBond();
                         Log.v(TAG, "Create Bound Status: " + statusCreateBound);
-                    } else {
-                        mBluetoothGatt.discoverServices();
-                        Log.v(TAG, "discover service again ");
                     }
 
             }
@@ -250,16 +228,13 @@ private final static String TAG = "BLE";
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    mScanning = false;
                     mBluetoothAdapter.stopLeScan(mLeScanCallback);
                 }
             }, SCAN_PERIOD);
 
-            mScanning = true;
             Log.v(TAG, "Bluetooth Scanning Start");
             mBluetoothAdapter.startLeScan(mLeScanCallback);
         } else {
-            mScanning = false;
             Log.v(TAG, "Bluetooth Scanning Stop");
             mBluetoothAdapter.stopLeScan(mLeScanCallback);
         }
@@ -268,33 +243,26 @@ private final static String TAG = "BLE";
     private void displayGattServices(List<BluetoothGattService> gattServices) {
         if (gattServices == null) return;
         String uuid = null;
-        String unknownServiceString = "UnKnownServices";
-        String unknownCharaString = "UnKnowCharactristic";
-
-
         for (BluetoothGattService gattService : gattServices) {
-            HashMap<String, String> currentServiceData =
-                    new HashMap<String, String>();
             uuid = gattService.getUuid().toString();
             Log.v(TAG, "\nService : " + uuid);
 
             List<BluetoothGattCharacteristic> gattCharacteristics =
                     gattService.getCharacteristics();
-            ArrayList<BluetoothGattCharacteristic> charas =
-                    new ArrayList<BluetoothGattCharacteristic>();
             // Loops through available Characteristics.
             for (BluetoothGattCharacteristic gattCharacteristic :
                     gattCharacteristics) {
                 uuid = gattCharacteristic.getUuid().toString();
                 Log.v(TAG, "Characteristic : " + uuid);
+                mBluetoothGatt.setCharacteristicNotification(gattCharacteristic, true);
 
                 if(uuid.equalsIgnoreCase("4D050081-766C-42C4-8944-42BC98FC2D09")) {
                     Log.v(TAG, "CharIdentify Initilize");
+                    int writeType = mCharCommandTx.getPermissions();
+                    Log.v(TAG, "Write Type : " + writeType);
+
                     mCharIdentify = gattCharacteristic;
 
-                    mCharIdentify.setValue(new byte[]{(byte) 0x01});
-                    boolean status = mBluetoothGatt.writeCharacteristic(mCharIdentify);
-                    Log.v(TAG, "Lock Identify : " + status);
                 }
 
                 if(uuid.equalsIgnoreCase("4D050082-766C-42C4-8944-42BC98FC2D09")) {
@@ -306,10 +274,14 @@ private final static String TAG = "BLE";
                     mCharCommandTx = gattCharacteristic;
                     Log.v(TAG, "CommandTx char initialize");
                 }
-
             }
-
         }
+        mCharIdentify.setValue(new byte[]{(byte) 0x01});
+        boolean status = mBluetoothGatt.writeCharacteristic(mCharIdentify);
+        Log.v(TAG, "Lock Identify : " + status);
+
+        mBluetoothGatt.readCharacteristic(mCharAuthentication);
+        mBluetoothGatt.setCharacteristicNotification(mCharAuthentication, true);
     }
 
     private final BroadcastReceiver mPairReceiver = new BroadcastReceiver() {
@@ -322,36 +294,30 @@ private final static String TAG = "BLE";
 
                 if (state == BluetoothDevice.BOND_BONDED && prevState == BluetoothDevice.BOND_BONDING) {
                     Log.v(TAG, "Device is Paired");
-                    BluetoothGattCharacteristic requestCert = mCharCommandTx;// new BluetoothGattCharacteristic(UUID.fromString("4D050017-766C-42C4-8944-42BC98FC2D09"), 0 , 0);
-                    requestCert.setValue(new byte[]{ 0x60, 0x00});
-                    requestCert.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
-                    boolean statusCertificate = mBluetoothGatt.writeCharacteristic(requestCert);
-                    Log.v(TAG, "Write commandCertificate into  4D050017 : " + statusCertificate);
+                    mCharCommandTx.setValue(new byte[]{(byte) 0x60, (byte)0x00});
+                    mCharCommandTx.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+                    boolean status60 = mBluetoothGatt.writeCharacteristic(mCharCommandTx);
 
-                    boolean statusReadCharAuth = mBluetoothGatt.
-                            readCharacteristic(mCharAuthentication);
-                    Log.v(TAG, "Read Authentication characteristics status : " + statusReadCharAuth);
+                    Log.v(TAG, "Write 60 commandCertificate into  4D050017 : " + status60);
 
-//                    byte[] commandChallenge = {(byte)61,(byte)00};
-//                    mCharCommandTx.setValue(new byte[]{ 0x61, 0x00});
-//                    mCharCommandTx.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
-//                    boolean statusChallenge = mBluetoothGatt.writeCharacteristic(mCharCommandTx);
-//                    Log.v(TAG, "Write commandChallenge into  4D050017 : " + statusChallenge);
-//
-//
-//                    byte[] commandSignatureSend = {0x63,0x20 ,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10,0x11,0x21,0x31,0x41,0x51,0x61,0x71,(byte) 0x81,(byte)0x91,(byte)0xA1,(byte)0xB1,(byte)0xC1,(byte)0xD1,(byte)0xE1,(byte)0xF2,0x0};
-//                    mCharCommandTx.setValue(commandSignatureSend);
-//                    mCharCommandTx.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
-//                    boolean statusSignatureSend = mBluetoothGatt.writeCharacteristic(mCharCommandTx);
-//                    Log.v(TAG, "Write commandSignature into  4D050017 : " + statusSignatureSend);
-//
-//
-//                    byte[] commandChallengeSend = {0x62,0x20 ,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10,0x11,0x21,0x31,0x41,0x51,0x61,0x71,(byte) 0x81,(byte)0x91,(byte)0xA1,(byte)0xB1,(byte)0xC1,(byte)0xD1,(byte)0xE1,(byte)0xF2,0x0};
-//                    mCharCommandTx.setValue(commandChallengeSend);
-//                    mCharCommandTx.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
-//                    boolean statusChallengeSend = mBluetoothGatt.writeCharacteristic(mCharCommandTx);
-//                    Log.v(TAG, "Write 6000 into  4D050017 : " + statusChallengeSend);
+                    BluetoothGattDescriptor descriptor = mCharCommandTx.getDescriptor(
+                            UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+                    descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                    mBluetoothGatt.writeDescriptor(descriptor);
 
+                    mHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+//                            mCharCommandTx.setValue(new byte[]{(byte) 0x61, (byte)0x00});
+//                            mCharCommandTx.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+//                            boolean status61 = mBluetoothGatt.writeCharacteristic(mCharCommandTx);
+//                            Log.v(TAG, "Write 61 commandCertificate into  4D050017 : " + status61);
+                        }
+                    }, SCAN_PERIOD);
+
+                    broadcastUpdate(mCharAuthentication);
+                    mBluetoothGatt.readCharacteristic(mCharAuthentication);
+                    broadcastUpdate(mCharAuthentication);
 
                 } else if (state == BluetoothDevice.BOND_NONE && prevState == BluetoothDevice.BOND_BONDED){
                     Log.v(TAG, "Device is Un Paired");
@@ -361,27 +327,16 @@ private final static String TAG = "BLE";
         }
     };
 
-    public static byte[] createByteArray(String str) {
-        int length = str.length();
-        byte[] bArr = new byte[(length / 2)];
-        for (int i = 0; i < length; i += 2) {
-            bArr[i / 2] = (byte) ((Character.digit(str.charAt(i), 16) << 4) + Character.digit(str.charAt(i + 1), 16));
+    private void broadcastUpdate(final BluetoothGattCharacteristic characteristic) {
+        // Writes the data formatted in HEX.
+        final byte[] data = characteristic.getValue();
+
+        if (data != null && data.length > 0) {
+            Log.v(TAG, "Raw Data : " + data.toString());
+            final StringBuilder stringBuilder = new StringBuilder(data.length);
+            for(byte byteChar : data)
+                stringBuilder.append(String.format("%02X ", byteChar));
+            Log.v(TAG, "Data in String : " + stringBuilder.toString());
         }
-        return bArr;
-    }
-
-    public static String encode(byte[] bytes) {
-        final int length = bytes.length;
-
-        // | BigInteger constructor throws if it is given an empty array.
-        if (length == 0) {
-            return "00";
-        }
-
-        final int evenLength = (int)(2 * Math.ceil(length / 2.0));
-        final String format = "%0" + evenLength + "x";
-        final String result = String.format (format, new BigInteger(bytes));
-
-        return result;
     }
 }
